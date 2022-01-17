d0fc9f8dec105ab19dcb6aa238ec3d8744826cce
Bogdan Barbu <bogdan.barbu@infoworld.ro>
2022-01-17 22:05:04 +0200

    wip

--- terapie-si-masaj/src/app/modules/appointment/appointment-routing.module.ts
+++ terapie-si-masaj/src/app/modules/appointment/appointment-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { AppointmentFormComponent } from "./components/appointment-form/appointment-form.component";
 import { AppointmentComponent } from "./container/appointment.component";
 
 @NgModule({
     imports: [
         RouterModule.forChild([{
             path: '',
-            component: AppointmentComponent
-        }])
+            component: AppointmentComponent,
+            children: [
+                {
+                    path: 'personal',
+                    component: AppointmentFormComponent
+                }
+            ]
+        }
+        
+    ])
     ]
 })
 export class AppointmentRoutingModule {
--- terapie-si-masaj/src/app/modules/appointment/components/appointment-form/appointment-form.component.ts
+++ terapie-si-masaj/src/app/modules/appointment/components/appointment-form/appointment-form.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+export const calendarRo = {
+  firstDayOfWeek: 1,
+  dayNames: ['Duminică', 'Luni', 'Marţi', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'],
+  dayNamesShort: ['Du', 'Lu', 'Ma', 'Mi', 'Jo', 'Vi', 'Sa'],
+  dayNamesMin: ['Du', 'Lu', 'Ma', 'Mi', 'Jo', 'Vi', 'Sa'],
+  dayCodes: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+  monthNames: ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'],
+  monthNamesShort: ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Iun', 'Iul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  dateFormat: 'dd.mm.yy',
+  today: 'Astăzi',
+  clear: 'Șterge'
+};
 @Component({
   selector: 'app-appointment-form',
   templateUrl: './appointment-form.component.html',
@@ -11,11 +22,16 @@ export class AppointmentFormComponent implements OnInit {
   personalInformation: any;
 
   submitted: boolean = false;
+  //interval: any[]
 
-  constructor( private router: Router) { }
+  constructor( private router: Router) { 
+    
+  }
 
   ngOnInit() { 
       //this.personalInformation = this.ticketService.getTicketInformation().personalInformation;
+      const date = new Date();
+      console.log(calendarRo.dayNames[date.getDay()], ' ', calendarRo.monthNames[date.getMonth()])
   }
 
   nextPage() {
